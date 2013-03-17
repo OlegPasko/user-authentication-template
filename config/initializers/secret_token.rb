d75ded57f6d3faf40fd0fa17d22aa21b8afbba78
Oleg Pasko <olegpasko@gmail.com>
2013-03-17 21:21:29 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turbik::Application.config.secret_key_base = '27b2b1b60b181cd8dedf47e3005b80c4fa8a5784fd7b7ca377f365f42f98f686ddae07fdbba4c60cce9787eeaf23ad60663bcb395ae7af9dffe4e667030213f2'
